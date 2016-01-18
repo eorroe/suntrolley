18b3df9562860968b1f74079e3dc1b61f4acec85
eorroe <eorroe@gmail.com>
2016-01-17 22:30:42 -0500

    remove second /api route

--- app.js
+++ app.js
@@ -87,12 +87,6 @@ app.get('/api', cors(), function(req, res) {
 	});
 });
 
-app.get('/api', function(req, res) {
-	getRoutes().then(function(jsonArr) {
-		res.send(jsonArr);
-	});
-});
-
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
 	console.log('SunTrolley Is Running On Port ' + port);
